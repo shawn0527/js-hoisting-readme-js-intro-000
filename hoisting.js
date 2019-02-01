5ffd0fe746a44f86b5abbbfd611ed9d6f4bb0dd2
Shawn Ren <doggy-ren@hotmail.com>
2019-02-01 03:40:58 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -8,18 +8,25 @@ function callMe() {
 }
 
 
+function crazy(){}
   // fix the code in here:
-
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
 thisIsCrazy();
-
-
+}
 
 function sayMyName() {
+  
   // fix the code in here:
+  var name='Cricky'
+
+  sayMy()
+
+  function sayMy(){
+    
 var name = "Kristin";
 console.log(name)
 }
-sayMyName()
+}
+
